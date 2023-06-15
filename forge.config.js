ef85d0c797f69eb14cbc0e69065d5c02137119c3
asutptec4 <asutp_tec4@tut.by>
2023-06-15 19:33:47 +0300

    chore: new version

--- forge.config.js
+++ forge.config.js
@@ -8,7 +8,7 @@ module.exports = {
     },
     {
       name: '@electron-forge/maker-zip',
-      platforms: ['darwin', 'linux'],
+      platforms: ['darwin', 'linux', 'win32'],
     },
     {
       name: '@electron-forge/maker-deb',
